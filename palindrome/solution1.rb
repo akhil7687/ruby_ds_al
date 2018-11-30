754b5a292c0bbc13edbf72783fb3f772b3c1bb45
AKHIL <puru@suphalaam.com>
2018-11-30 12:52:37 +0530

    fix

--- palindrome/solution1.rb
+++
@@ -1,7 +0,0 @@
-# if string reverse == string its called palindrome
-
-def palindrome(str)
-  str == str.reverse
-end
-
-puts palindrome("akhka")
--- string_reverse/solution1.rb
+++
@@ -1,9 +0,0 @@
-# using built in function reverse
-# also can be done by just calling "reverse" method on string
-
-def reverse(str)
-  str.split("").reverse().join()
-end
-
-str = "Akhil"
-puts reverse(str)
